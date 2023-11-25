4f1dc8ddf8c88c71e48c7c5b14005962e93888ad
Виталий Садиков <vitaly.sadikov1@yandex.ru>
2023-11-25 14:03:05 +0500

    Add profile

--- src/main/java/ru/okei/urbaton/controllers/MainController.java
+++ src/main/java/ru/okei/urbaton/controllers/MainController.java
@@ -1,18 +1,22 @@
 package ru.okei.urbaton.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.okei.urbaton.dao.PersonDAO;
 import ru.okei.urbaton.models.Person;
 
+
 @Controller
 public class MainController {
     boolean auth = false; // авторизация
-    String[] personData;
+    String[] personData; // данные о пользователе
+    String[] fio;
     @Autowired
     PersonDAO personDAO;
 
@@ -22,26 +26,52 @@ public class MainController {
         model.addAttribute("auth", auth);
         return "main";
     }
+
     // Личный кабинет
     @GetMapping("/profile")
-    public String profile() {
-        return "profile";
+    public String profile(Model model) {
+        model.addAttribute("name", fio[0] + " " + fio[1]);
+        return "UserLkNews";
     }
 
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
+
     @PostMapping("/registration")
-    public String registrationPost(@ModelAttribute("person") Person person){
-        auth = personDAO.save(person);
-        return "main";
+    public String registrationPost(@ModelAttribute("person") @Valid Person person,
+                                   BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            return "registration";
+        }
+        personDAO.save(person);
+        return "redirect:/";
     }
 
     @PostMapping("/login")
-    public String loginPost(@ModelAttribute Person person){
-        auth = personDAO.login(person);
-        personData = personDAO.getData(person);
-        return "main";
+    public String loginPost(@ModelAttribute Person person,
+                            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            return "registration";
+        }
+        auth = personDAO.login(person); // меняем вход на true
+        personData = personDAO.getData(person); // получаем данные о пользователе
+        fio = personData[0].split(" "); // ФИО
+        System.out.println(personData);
+        return "redirect:/";
+    }
+    @GetMapping("/new")
+    public String nw() {
+        return "new";
+    }
+    @PostMapping("/new")
+    public String newPost(@ModelAttribute Person person,
+                            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            return "new";
+        }
+        System.out.println("ia");
+        return "redirect:/";
     }
 }
--- src/main/java/ru/okei/urbaton/models/Person.java
+++ src/main/java/ru/okei/urbaton/models/Person.java
@@ -1,7 +1,45 @@
 package ru.okei.urbaton.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+
 public class Person {
-    private String name, email, password, naprav, typeProfile;
+    @NotEmpty(message = "1")
+    @Size(min=3, max=45, message = "Длина не более 45 символов")
+    private String name;
+    @NotEmpty(message = "Поле не может быть пустым")
+    @Email(message = "1")
+    private String email;
+    @NotEmpty(message = "1")
+    private String password;
+    @NotEmpty(message = "Поле не может быть пустым")
+    private String confrimPassword;
+    @NotEmpty(message = "Поле не может быть пустым")
+    private String naprav;
+    private String typeProfile;
+
+    public Person(){
+
+    }
+
+    public Person(String name, String email, String password, String confrimPassword, String naprav, String typeProfile) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.confrimPassword = confrimPassword;
+        this.naprav = naprav;
+        this.typeProfile = typeProfile;
+    }
+
+    public String getConfrimPassword() {
+        return confrimPassword;
+    }
+
+    public void setConfrimPassword(String confrimPassword) {
+        this.confrimPassword = confrimPassword;
+    }
 
     public String getName() {
         return name;
